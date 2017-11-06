d7de9e89503a9d9005928642bef8544af08f31de
Ness Bokobza <bokoness@gmail.com>
2017-11-06 21:36:08 +0200

    states

---
+++ 0-playground/app.js
@@ -0,0 +1,154 @@
+'use strict';
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+/*
+     A demo that shoes how react rendering works 
+        - everytime the state is changed react is rerender elements
+        - just like we are doing here with renderCounterApp()
+    - the reason react dont rerender elements here, and we need to rerender them with renderCounterApp() - is we didnt bind functions;
+*/
+
+console.log('states');
+
+var count = 0;
+var addOne = function addOne() {
+    count++;
+    renderCounterApp();
+};
+
+var minusOne = function minusOne() {
+    count--;
+    renderCounterApp();
+};
+
+var reset = function reset() {
+    count = 0;
+    renderCounterApp();
+};
+
+var appRoot = document.getElementById('app');
+
+var renderCounterApp = function renderCounterApp() {
+    var templateTwo = React.createElement(
+        'div',
+        null,
+        React.createElement(
+            'h1',
+            null,
+            'Count: ',
+            count
+        ),
+        React.createElement(
+            'button',
+            { onClick: addOne },
+            '+1'
+        ),
+        React.createElement(
+            'button',
+            { onClick: minusOne },
+            '-1'
+        ),
+        React.createElement(
+            'button',
+            { onClick: reset },
+            'reset'
+        )
+    );
+
+    //takes 2 args, 1: the element to render, 2 the container element in html file
+    ReactDOM.render(templateTwo, appRoot);
+};
+
+renderCounterApp();
+
+//************** A a way to do the same app with Reactjs states ******************/
+//everytime the state changes - the elements that are using the state will rerender automaticly, behind the seens the renderCounterApp() method is called
+
+var Counter = function (_React$Component) {
+    _inherits(Counter, _React$Component);
+
+    function Counter(props) {
+        _classCallCheck(this, Counter);
+
+        var _this = _possibleConstructorReturn(this, (Counter.__proto__ || Object.getPrototypeOf(Counter)).call(this, props));
+
+        _this.addOne = _this.addOne.bind(_this);
+        _this.minusOne = _this.minusOne.bind(_this);
+        _this.reset = _this.reset.bind(_this);
+
+        _this.state = {
+            count: 0
+        };
+        return _this;
+    }
+    //the prefred way to setState - pass a function to setState
+    //params: 1st - the previous state (before it got updated)
+
+
+    _createClass(Counter, [{
+        key: 'addOne',
+        value: function addOne() {
+            this.setState(function (prevState) {
+                return {
+                    count: prevState.count + 1
+                };
+            });
+        }
+        //another way to setState, but its less preffered (watch lecture 34)
+
+    }, {
+        key: 'minusOne',
+        value: function minusOne() {
+            this.setState({ count: this.state.count - 1 });
+        }
+    }, {
+        key: 'reset',
+        value: function reset() {
+            this.setState(function (prev) {
+                return {
+                    count: 0
+                };
+            });
+        }
+    }, {
+        key: 'render',
+        value: function render() {
+            return React.createElement(
+                'div',
+                null,
+                React.createElement(
+                    'h1',
+                    null,
+                    'Count: ',
+                    this.state.count
+                ),
+                React.createElement(
+                    'button',
+                    { onClick: this.addOne },
+                    '+1'
+                ),
+                React.createElement(
+                    'button',
+                    { onClick: this.minusOne },
+                    '-1'
+                ),
+                React.createElement(
+                    'button',
+                    { onClick: this.reset },
+                    'reset'
+                )
+            );
+        }
+    }]);
+
+    return Counter;
+}(React.Component);
+
+ReactDOM.render(React.createElement(Counter, null), document.getElementById('app'));
