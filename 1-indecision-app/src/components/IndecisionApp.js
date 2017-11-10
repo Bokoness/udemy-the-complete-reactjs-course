95156fa4d032d1ed0a8f21caca2afab06ce63afe
Bokoness <bokoness@gmail.com>
2017-11-10 11:51:46 +0200

    end of section 6: adding babel plugin - class-properties-syntax

--- 1-indecision-app/src/components/IndecisionApp.js
+++ 1-indecision-app/src/components/IndecisionApp.js
@@ -6,17 +6,35 @@ import Options from './Options';
 import AddOption from './AddOption';
 
 class IndecisionApp extends React.Component {
-    constructor(props) {
-        super(props);
 
-        this.handleDeleteOptions = this.handleDeleteOptions.bind(this);
-        this.handleDeleteOption = this.handleDeleteOption.bind(this);
-        this.handlePick = this.handlePick.bind(this);
-        this.handleAddOption = this.handleAddOption.bind(this);
+    state = {
+        options: []
+    }
 
-        this.state = {
-            options: []
+    handleDeleteOptions = () => {
+        this.setState(() => ({ options: []}));
+    }
+
+    handleDeleteOption = (optionToRemove) => {
+        this.setState((prevState) => ({
+            options: prevState.options.filter((option) => optionToRemove !== option)
+        }));
+    }
+
+    handlePick = () => {
+        const randomNum = Math.floor(Math.random() * this.state.options.length);
+        const option = this.state.options[randomNum];
+        alert(option);
+    }
+    
+    handleAddOption = (option) => {
+        if(!option) {
+            return 'Enter valid value to add item';
+        } else if(this.state.options.indexOf(option) > -1) { //check if userinput is already exists
+            return 'This option already exists'
         }
+
+        this.setState((prevState) => ({options: prevState.options.concat(option)}));
     }
 
     componentDidMount() {
@@ -36,32 +54,6 @@ class IndecisionApp extends React.Component {
             const json = JSON.stringify(this.state.options);
             localStorage.setItem('options' ,json);
         };
-    }
-
-    handleDeleteOptions () {
-        this.setState(() => ({ options: []}));
-    }
-
-    handleDeleteOption (optionToRemove) {
-        this.setState((prevState) => ({
-            options: prevState.options.filter((option) => optionToRemove !== option)
-        }));
-    }
-
-    handlePick() {
-        const randomNum = Math.floor(Math.random() * this.state.options.length);
-        const option = this.state.options[randomNum];
-        alert(option);
-    }
-    
-    handleAddOption(option) {
-        if(!option) {
-            return 'Enter valid value to add item';
-        } else if(this.state.options.indexOf(option) > -1) { //check if userinput is already exists
-            return 'This option already exists'
-        }
-
-        this.setState((prevState) => ({options: prevState.options.concat(option)}));
     }
 
     render() {
