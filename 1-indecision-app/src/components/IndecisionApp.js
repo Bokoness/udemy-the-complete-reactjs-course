1314fc6b641f320cc9bcf309d062af3e990a047e
Ness Bokobza <bokoness@gmail.com>
2017-11-11 21:51:13 +0200

    adding third party library: react-modal. end of section  7

--- 1-indecision-app/src/components/IndecisionApp.js
+++ 1-indecision-app/src/components/IndecisionApp.js
@@ -4,11 +4,12 @@ import Header from './Header';
 import Action from './Action';
 import Options from './Options';
 import AddOption from './AddOption';
-
+import OptionModal from './OptionsModal';
 class IndecisionApp extends React.Component {
 
     state = {
-        options: []
+        options: [],
+        selectedOption: undefined
     }
 
     handleDeleteOptions = () => {
@@ -24,7 +25,15 @@ class IndecisionApp extends React.Component {
     handlePick = () => {
         const randomNum = Math.floor(Math.random() * this.state.options.length);
         const option = this.state.options[randomNum];
-        alert(option);
+        this.setState(() => ({
+            selectedOption: option
+        }))
+    }
+
+    handleClearSelectedOption = () => {
+        this.setState(() => ({
+            selectedOption: undefined
+        }));
     }
     
     handleAddOption = (option) => {
@@ -72,6 +81,11 @@ class IndecisionApp extends React.Component {
                 />
                 <AddOption
                     handleAddOption={this.handleAddOption}
+                />
+
+                <OptionModal
+                    selectedOption={this.state.selectedOption}
+                    handleClearSelectedOption={this.handleClearSelectedOption}
                 />
             </div>
         )
