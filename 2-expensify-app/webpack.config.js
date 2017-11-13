e03b4f69a308a296e972e8b4bf420c4a51212fc4
Ness Bokobza <bokoness@gmail.com>
2017-11-13 20:45:15 +0200

    end of section 79 adding switch to routing adding 404 message to routes that doesnt exists

--- 2-expensify-app/webpack.config.js
+++ 2-expensify-app/webpack.config.js
@@ -22,6 +22,7 @@ module.exports = {
   },
   devtool: 'cheap-module-eval-source-map',
   devServer: {
-    contentBase: path.join(__dirname, 'public')
+    contentBase: path.join(__dirname, 'public'),
+    historyApiFallback: true //the react-route will be incharge of routing and not devserver
   }
 };
