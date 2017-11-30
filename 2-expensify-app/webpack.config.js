5efca179063ea508247a8f5fbf2bdcf5d23ff86e
Ness Bokobza <bokoness@gmail.com>
2017-11-30 22:38:58 +0200

    sec 13 lec 134 - configuring webpack for prodction

--- 2-expensify-app/webpack.config.js
+++ 2-expensify-app/webpack.config.js
@@ -1,28 +1,36 @@
 const path = require('path');
 
-module.exports = {
-  entry: './src/app.js',
-  output: {
-    path: path.join(__dirname, 'public'),
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [{
-      loader: 'babel-loader',
-      test: /\.js$/,
-      exclude: /node_modules/
-    }, {
-      test: /\.s?css$/,
-      use: [
-        'style-loader',
-        'css-loader',
-        'sass-loader'
-      ]
-    }]
-  },
-  devtool: 'cheap-module-eval-source-map',
-  devServer: {
-    contentBase: path.join(__dirname, 'public'),
-    historyApiFallback: true//the react-route will be incharge of routing and not devserver
+module.exports = (env) => {
+  //env - a var that passed from package.json (with string production)
+  //we use it so we can manipulate webpack different for production
+  console.log('env:' ,env)
+  const isProduction = env === 'production';
+
+  return {
+    entry: './src/app.js',
+    output: {
+      path: path.join(__dirname, 'public'),
+      filename: 'bundle.js'
+    },
+    module: {
+      rules: [{
+        loader: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/
+      }, {
+        test: /\.s?css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      }]
+    },
+    devtool: isProduction? 'source-map' : 'cheap-module-eval-source-map',
+    devServer: {
+      contentBase: path.join(__dirname, 'public'),
+      historyApiFallback: true//the react-route will be incharge of routing and not devserver
+    }
   }
-};
+}
+
