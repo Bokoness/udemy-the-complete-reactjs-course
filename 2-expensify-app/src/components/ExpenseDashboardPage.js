bfe2acd290cb5f283f3177cca77627178a8838b4
Ness Bokobza <bokoness@gmail.com>
2017-12-01 15:42:46 +0200

    add expense summary to dashboard

--- 2-expensify-app/src/components/ExpenseDashboardPage.js
+++ 2-expensify-app/src/components/ExpenseDashboardPage.js
@@ -1,15 +1,14 @@
 import React from 'react';
-import ExpenseList from './ExpensList';
+import ExpenseList from './ExpenseList';
 import ExpenseListFilters from './ExpenseListFilters';
+import ExpensesSummary from './ExpensesSummary';
 
-const ExpenseDashboardPage = (props) => {
-    console.log(props);
-    return (
-        <div>
-            <ExpenseListFilters/>
-            <ExpenseList/>
-        </div>
-    )
-}
+const ExpenseDashboardPage = () => (
+  <div>
+    <ExpensesSummary />
+    <ExpenseListFilters />
+    <ExpenseList />
+  </div>
+);
 
 export default ExpenseDashboardPage;
